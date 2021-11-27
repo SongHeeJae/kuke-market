2ea8d58f102c0a0f734cd0ae5c0e47fa122ab064
SongHeeJae <gmlwo308@gmail.com>
2021-11-27 17:15:16 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/kukekyakya/kukemarket/KukemarketApplication.java
@@ -0,0 +1,13 @@
+package kukekyakya.kukemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KukemarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kukekyakya/kukemarket/KukemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kukekyakya.kukemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KukemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
