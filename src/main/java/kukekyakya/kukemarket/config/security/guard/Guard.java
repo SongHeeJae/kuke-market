4aee4a85c56eef7d704181d5cd483b4c75161d79
SongHeeJae <gmlwo308@gmail.com>
2021-12-26 15:23:58 +0900

    refactor: Guard 로직, 추상 클래스를 이용한 템플릿 메소드로 변경

--- src/main/java/kukekyakya/kukemarket/config/security/guard/Guard.java
+++ src/main/java/kukekyakya/kukemarket/config/security/guard/Guard.java
@@ -1,5 +1,18 @@
 package kukekyakya.kukemarket.config.security.guard;
 
-public interface Guard {
-    boolean check(Long id);
+import kukekyakya.kukemarket.entity.member.RoleType;
+
+import java.util.List;
+
+public abstract class Guard {
+    public final boolean check(Long id) {
+        return AuthHelper.isAuthenticated() && (hasRole(getRoleTypes()) || isResourceOwner(id));
+    }
+
+    abstract protected List<RoleType> getRoleTypes();
+    abstract protected boolean isResourceOwner(Long id);
+
+    private boolean hasRole(List<RoleType> roleTypes) {
+        return roleTypes.stream().allMatch(roleType -> AuthHelper.extractMemberRoles().contains(roleType));
+    }
 }
